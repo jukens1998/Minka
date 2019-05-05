cf2df2528d3ef8a48c5035ba9c2ce8ce303bcbe4
juan diego tovaria castro <juken.555@hotmail.com>
2019-05-05 11:13:46 -0500

    pruebas

--- minka/src/app/app.module.ts
+++ minka/src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppComponent } from './app.component';
 import { TransaccionesComponent } from './transacciones/transacciones.component';
 import {Routes, RouterModule} from '@angular/router';
 import { MovimientosComponent } from './movimientos/movimientos.component';
+import { JuegosComponent } from './juegos/juegos.component';
 const appRoutes: Routes =[
   {path: 'home', component: TransaccionesComponent},
   {path: '', component: TransaccionesComponent },
-  {path: 'movimientos', component: MovimientosComponent}
+  {path: 'movimientos', component: MovimientosComponent},
+  {path: 'juegos', component: JuegosComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     TransaccionesComponent,
-    MovimientosComponent
+    MovimientosComponent,
+    JuegosComponent
   ],
   imports: [
     BrowserModule,
