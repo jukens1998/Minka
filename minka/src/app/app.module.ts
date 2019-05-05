a983cd95e3ee5c9ec2aa5de421cc42d7cce1b04b
juan diego tovaria castro <juken.555@hotmail.com>
2019-05-05 01:40:08 -0500

    Transacciones y principal

--- minka/src/app/app.module.ts
+++ minka/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransaccionesComponent } from './transacciones/transacciones.component';
+import {Routes, RouterModule} from '@angular/router';
+import { MovimientosComponent } from './movimientos/movimientos.component';
+const appRoutes: Routes =[
+  {path: 'home', component: TransaccionesComponent},
+  {path: '', component: TransaccionesComponent },
+  {path: 'movimientos', component: MovimientosComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransaccionesComponent,
+    MovimientosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ minka/src/app/transacciones/transacciones.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../../assets/canvasjs.min.js';
+
+@Component({
+  selector: 'app-transacciones',
+  templateUrl: './transacciones.component.html',
+  styleUrls: ['./transacciones.component.css']
+})
+export class TransaccionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    });
+      
+    chart.render();
+      }
+}
